d2e587e504362bd7bd6d73789375e32262ac6fc9
Imran Razak <Imranrazak1@icloud.com>
2022-01-11 13:50:40 +0000

    Initial Commit

---
+++ BakeOff/BakeOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeOffApp.swift
+//  BakeOff
+//
+//  Created by Imran razak on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BakeOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
